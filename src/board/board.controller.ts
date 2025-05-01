85614bb1899716101acbfeef9c5358c9aeb5e3aa
seonghunlee94 <godloveme94@naver.com>
2025-05-01 12:48:51 +0900

    add BoardController with basic CRUD operations and corresponding tests

---
+++ src/board/board.controller.ts
@@ -0,0 +1,41 @@
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+
+@Controller('board')
+export class BoardController {
+
+
+    @Get()
+    findAll() {
+        return 'findAll';   
+    }
+
+    @Get(':id')
+    find(
+        @Param('id') id: number
+    ) {
+        return `find ${id}`;   
+    }
+
+    @Post()
+    create(
+        @Body() data
+    ) {
+        return 'create';   
+    }
+
+    @Put(':id')
+    update(
+        @Param('id') id: number,
+        @Body() data
+    ) {
+        return `update ${id}`;   
+    }
+
+    @Delete(':id')
+    remove(
+        @Param('id') id: number 
+    ) {
+        return `remove ${id}`;   
+    }
+
+}
