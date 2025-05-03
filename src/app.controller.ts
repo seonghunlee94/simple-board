a848990ea023c774baec5466092e2769d6bdaf0c
seonghunlee94 <godloveme94@naver.com>
2025-05-03 22:42:01 +0900

    refactor: update HTTP requests in .http file and enhance AppController with new methods and middleware integration

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus, Logger, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Ip } from './decorators/ip.decorator';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
+  constructor(
+    private readonly appService: AppService,
+    private readonly configService: ConfigService,
+  ) {}
+  
+  // private readonly logger = new Logger(AppController.name);
   @Get()
-  getHello(): string {
+  getHello(
+    // @Ip() ip: string,
+  ): string {
+    // console.log(ip);
+    // this.logger.log('log 테스트');
+    // this.logger.warn('warn 테스트');
+    // this.logger.debug('debug 테스트');
+    // this.logger.error('error 테스트');
+    // this.logger.verbose('verbose 테스트');
+
+    console.log(this.configService.get('ENVIRONMENT'));
     return this.appService.getHello();
+    // throw new HttpException('Not Found', HttpStatus.NOT_FOUND);
+  }
+
+  @Get('name')
+  getName(
+    // @Param('name') name: string
+    @Query('name') name: string
+  ): string {
+    return `${name} hello`;
   }
 }
